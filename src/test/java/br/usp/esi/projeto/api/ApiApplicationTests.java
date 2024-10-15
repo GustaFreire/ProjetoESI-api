a232297fcf859a699b6e0dda0e7a2084938ac023
Gustavo Freire Pereira <gustavo_freire18@usp.br>
2024-10-14 23:00:40 -0300

    first commit

---
+++ src/test/java/br/usp/esi/projeto/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usp.esi.projeto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
