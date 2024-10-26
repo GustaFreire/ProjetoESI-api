d2f589fe66ff80a9328546643b0c4151c0172077
Gustavo Freire Pereira <gustavo_freire18@usp.br>
2024-10-26 00:51:14 -0300

    c

--- src/main/java/br/usp/esi/api/domain/repository/CcpRepository.java
+++ src/main/java/br/usp/esi/api/domain/repository/CcpRepository.java
@@ -3,7 +3,6 @@ package br.usp.esi.api.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import br.usp.esi.api.domain.model.Aluno;
 import br.usp.esi.api.domain.model.Ccp;
 
 public interface CcpRepository extends JpaRepository<Ccp, Long> {
